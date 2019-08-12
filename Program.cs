9f08c6a07d33d3b26bd2e9e13c678e0790052476
Jimmy Mead <jdmead@asu.edu>
2019-08-12 15:44:38 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Human
+{
+    class Human
+    {
+       public string Name { get; set; }
+       public int Strength { get; set; } = 3;
+       public int Intelligence { get; set; } = 3;
+       public int Dexterity { get; set; } = 3;
+       private int Health { get; set; } = 100;
+
+       public Human(string name)
+       {
+           Name = name;
+       }
+
+       public Human(string name, int strength, int intelligence, int dexterity, int health) :this(name)
+       {
+           Strength = strength;
+           Intelligence = intelligence;
+           Dexterity = dexterity;
+           Health = health;
+       }
+
+       public void Attack(Object victim)
+       {
+           var Vic = (Human) victim;
+           Vic.Health -= 5 * Strength;
+           Console.ForegroundColor = ConsoleColor.Red;
+           Console.WriteLine($"ATTACK! {Vic.Name} loses {5 * Strength } pts from their Health!");
+           Console.WriteLine("Current Victim Stats: " + Vic.ToString() );
+       }
+       public override string ToString()
+       {
+           return
+            $"Name: {Name}, Strength: {Strength}, Intelligence: {Intelligence}, Dexterity: {Dexterity}, Health: {Health}";
+       }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player1 = new Human("Beth");
+            var player2 = new Human("Mikayla", 10, 50, 5, 20);
+            Console.WriteLine(player1);
+            Console.WriteLine(player2);
+            player2.Attack(player1);
+
+        }
+    }
+
+}
